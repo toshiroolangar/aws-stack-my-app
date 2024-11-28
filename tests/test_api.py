9ab77448b684360f43650c55e179e8fcb2da68e4
cka-user <toshi.olangar04@gmail.com>
2024-11-28 20:40:42 +0800

    Fixed test

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,8 +2,6 @@ import pytest
 from app import app, products
 
 def test_get_products():
-    response = app.test_client().get('/products')
+    response = app.test_client().get('/')
     assert response.status_code == 200
     data = response.json
-    assert len(data) == 2
-    assert data[0]['Name'] == 'Gago'
