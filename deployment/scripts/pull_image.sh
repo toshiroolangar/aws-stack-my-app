52941690bfd204f24c32cb33d31509040c9208c5
cka-user <toshi.olangar04@gmail.com>
2024-12-04 20:33:31 +0800

    Fixed deployment script pull_image region

--- deployment/scripts/pull_image.sh
+++ deployment/scripts/pull_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #Login to the repo
-aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.${region}.amazonaws.com
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin ${account_id}.dkr.ecr.${region}.amazonaws.com
 #Pull the image
 docker pull ${ECR_REPO}:{BUILD_DATE}
