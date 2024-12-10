2ad875075083276f0e9f2fe1ccec7cbb2b4506db
cka-user <toshi.olangar04@gmail.com>
2024-12-10 13:40:49 +0800

    Fixed stop_container.sh

--- deployment/scripts/stop_container.sh
+++ deployment/scripts/stop_container.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-running_app=`docker ps | grep "aws=devops" | wc -l `
+running_app=`docker ps | grep "aws=devops" | wc -l`
 
 if [[ $running_app -eq 1 ]]; then
     #Stop Container aws-devops
     docker ps -aq --filter "name=aws-devops" | xargs docker stop
     #Remove container aws-devops
     docker ps -aq --filter "name=aws-devops" | xargs docker rm
-else echo "There are no running containers"
+else 
+    echo "There are no running containers"
 fi
