c750161b2ca28e6275ac50c6b071b5b831718470
cka-user <toshi.olangar04@gmail.com>
2024-12-10 22:47:03 +0800

    Added image_uri variable to run image

--- deployment/scripts/start_container.sh
+++ deployment/scripts/start_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+image_uri=`jq .ImageURI imageDefinitions.json  | sed -E 's/"//g'`
 #Run container 
-
 docker run -d \
   --name=aws-devops \
   -p 80:5000 \
-  ${ECR_REPO}:{BUILD_DATE}
+  $image_uri
