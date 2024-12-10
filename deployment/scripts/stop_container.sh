8caabcd9bec09e6fd442bb3948783e3240569db1
cka-user <toshi.olangar04@gmail.com>
2024-12-11 00:02:47 +0800

    Changed logic of kill contianer

--- deployment/scripts/stop_container.sh
+++ deployment/scripts/stop_container.sh
@@ -2,7 +2,7 @@
 #Stop Container aws-devops test
 running_app=`docker ps | grep "aws-devops" | wc -l`
 
-if [[ $running_app -eq 1 ]]; then
+if [[ $running_app -ne 0 ]]; then
     #Stop Container aws-devops
     docker ps -aq --filter "name=aws-devops" | xargs docker stop
     #Remove container aws-devops
