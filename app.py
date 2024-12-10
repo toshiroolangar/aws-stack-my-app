e11953d3564e7d522a02edab40ddba9a146ba4dc
cka-user <toshi.olangar04@gmail.com>
2024-12-11 00:17:42 +0800

    Update app.py to v2

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def welcome():
     """Return welcome message in root path"""
-    return 'Welcome to the jungle!'
+    return 'Welcome to the jungle! V2 Update with Inday'
 
 if __name__ == ('__main__'):
     app.run(host='0.0.0.0', port=5000)
