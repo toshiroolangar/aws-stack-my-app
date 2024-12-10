bb173fd5635a4e3b5c2af79aee0a9c7ff4d66bfd
cka-user <toshi.olangar04@gmail.com>
2024-12-10 22:58:59 +0800

    Added cd to ec2-user home dir to get image uri from imagedefinition.json file

--- deployment/scripts/pull_image.sh
+++ deployment/scripts/pull_image.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Login to the repo
 image_uri=`jq .ImageURI imageDefinitions.json  | sed -E 's/"//g'`
+cd /home/ec2-user/app/scripts/
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 167295297769.dkr.ecr.us-east-2.amazonaws.com/testspace/aws-devops
 #Pull the image
 docker pull $image_uri
