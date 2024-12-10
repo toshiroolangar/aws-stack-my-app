a6a76574a1214cb79368b036149bc47ab8e79352
cka-user <toshi.olangar04@gmail.com>
2024-12-10 11:27:41 +0800

    Changed the order of appspec hooks and added validation in stop container

--- deployment/scripts/stop_container.sh
+++ deployment/scripts/stop_container.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-#Stop Container aws-devops
-docker ps -aq --filter "name=aws-devops" | xargs docker stop
-#Remove container aws-devops
-docker ps -aq --filter "name=aws-devops" | xargs docker rm
+running_app=`docker ps -aq | wc -l`
+
+if [[ $running_app -eq 1 ]]; then
+    #Stop Container aws-devops
+    docker ps -aq --filter "name=aws-devops" | xargs docker stop
+    #Remove container aws-devops
+    docker ps -aq --filter "name=aws-devops" | xargs docker rm
+else echo "There are no running containers"
