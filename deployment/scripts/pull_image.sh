60a0d1420a3b2e904d3c7493ef02489a19d5bf80
cka-user <toshi.olangar04@gmail.com>
2024-12-10 14:37:12 +0800

    Revert "Added imageURI variable"
    This reverts commit 07f9d5e0847b9ad334de94ef8e6a71b9f958b287.

--- deployment/scripts/pull_image.sh
+++ deployment/scripts/pull_image.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #Login to the repo
-
-image_URI=`jq .ImageURI imageDefinitions.json  | sed -E 's/"//g'`
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 167295297769.dkr.ecr.us-east-2.amazonaws.com/testspace/aws-devops
 #Pull the image
-docker pull $image_URI
+docker pull ${ECR_REPO}:{BUILD_DATE}
