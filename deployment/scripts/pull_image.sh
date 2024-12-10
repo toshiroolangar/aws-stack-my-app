c5004d39b60afef83e6cb4e5432e5217be545e55
cka-user <toshi.olangar04@gmail.com>
2024-12-10 22:28:18 +0800

    Added image_uri variable in pull image script

--- deployment/scripts/pull_image.sh
+++ deployment/scripts/pull_image.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #Login to the repo
+image_uri=`jq .ImageURI imageDefinitions.json  | sed -E 's/"//g'`
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 167295297769.dkr.ecr.us-east-2.amazonaws.com/testspace/aws-devops
 #Pull the image
-docker pull ${ECR_REPO}:{BUILD_DATE}
+docker pull $image_uri
